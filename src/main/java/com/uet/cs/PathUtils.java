fce3f19b50e4e4f8f4a71d029ec75be8f7ceb73d
qkhanh2907 <25021824@vnu.edu.vn>
2026-04-27 16:13:27 +0700

    Push lan 2: Refactor su dung java.nio.file.Path

--- src/main/java/com/uet/cs/PathUtils.java
+++ src/main/java/com/uet/cs/PathUtils.java
@@ -1,8 +1,21 @@
+//package com.uet.cs;
+//
+//public class PathUtils {
+//    public String createLogFilePath(String directory, String filename) {
+//        // CỐ TÌNH CODE LỖI: Dùng cứng dấu gạch chéo của Linux/Mac
+//        return directory + "/" + filename;
+//    }
+//}
+
 package com.uet.cs;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PathUtils {
     public String createLogFilePath(String directory, String filename) {
-        // CỐ TÌNH CODE LỖI: Dùng cứng dấu gạch chéo của Linux/Mac
-        return directory + "/" + filename;
+        // REFACTOR: Sử dụng java.nio.file.Paths để hệ điều hành tự quyết định dấu gạch chéo
+        Path path = Paths.get(directory, filename);
+        return path.toString();
     }
 }
